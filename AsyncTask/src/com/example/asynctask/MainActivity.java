e89826c9c651674fa0792a41b91e48c843abb13f
fedejo <joelgc20@gmail.com>
2015-02-12 16:34:50 +0100

    Commit

---
+++ AsyncTask/src/com/example/asynctask/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.asynctask;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private EditText entrada;
+	private TextView salida;
+	
+	class MiThread extends Thread {
+		private int n, res;
+		
+		public MiThread(int n) {
+			this.n = n;
+		}
+		
+		@Override
+		public void run() {
+			res = factorial(n);
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					System.out.println("2 - " + n);
+					System.out.println("Res - " + res);
+					salida.append(n + "! = ");
+					salida.append(res + "\n");
+					
+				}
+			});
+		}
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		entrada = (EditText) findViewById(R.id.entrada);
+		salida = (TextView) findViewById(R.id.salida);
+	}
+	
+	public void calcularOperacion(View view) {
+		int n = Integer.parseInt(entrada.getText().toString());
+		salida.setText("");
+		MiTarea tarea = new MiTarea();
+        tarea.execute(n);
+	}
+	
+	public int factorial(int n) {
+		int res = 1;
+		for (int i=1; i<=n; i++) {
+			res *= i;
+		}
+		SystemClock.sleep(500);
+		return res;
+	}
+	
+	class MiTarea extends AsyncTask<Integer, Integer, Integer> {
+        private ProgressDialog progreso;
+        @Override 
+        protected void onPreExecute() {
+              progreso = new ProgressDialog(MainActivity.this);
+              progreso.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+              progreso.setMessage("Calculando...");
+              progreso.setCancelable(true);
+            
+              progreso.setOnCancelListener(new OnCancelListener() {
+                   public void onCancel(DialogInterface dialog) {
+                       MiTarea.this.cancel(true);
+                   }
+               });
+              
+              progreso.setMax(100);
+              progreso.setProgress(0);
+              progreso.show();
+        }
+
+        @Override 
+        protected Integer doInBackground(Integer... n) {
+               int res = 1;
+               for (int i = 1; i <= n[0] && !isCancelled(); i++) {
+                      res *= i;
+                      SystemClock.sleep(1000);
+                      publishProgress(i*100 / n[0]);
+               }
+
+               return res;
+        }
+
+        @Override 
+        protected void onProgressUpdate(Integer... porc) {
+               progreso.setProgress(porc[0]);
+        }
+
+        @Override 
+        protected void onPostExecute(Integer res) {
+               progreso.dismiss();
+               salida.append(res + "\n");
+        }
+        
+        @Override 
+        protected void onCancelled() {
+            salida.append("cancelado\n");
+        }
+
+	}
+}
